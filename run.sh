c117b7252972cf71bd78a545d7888536cef5099c
Grimlace <da.enmanuel@proton.me>
2026-06-03 15:16:16 -0500

    Delete unused apps

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-stow -Rv images yazi bat nix bash fastfetch fish fuzzel ghostty kitty niri noctalia nushell nvim starship
+stow -Rv images yazi bat nix bash fastfetch fish ghostty niri noctalia nushell nvim starship
